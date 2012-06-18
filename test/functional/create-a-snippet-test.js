4f27b43c4492204b832dc6c566d08976ad63f33e
James Newbery <james@tinnedfruit.com>
2012-06-18 16:09:42 +0100

    CasperJS functional test for creating a snippet
    Test currently fails because nothing has been written

---
+++ test/functional/create-a-snippet-test.js
@@ -0,0 +1,27 @@
+var baseUrl = "http://localhost:3000";
+
+casper.test.comment("Scenario: A user can create a snippet");
+
+casper.test.comment("Given I am on the home page");
+
+casper.start(baseUrl, function() {
+  this.test.comment("When I fill out the form");
+  this.fill("form.create-snippet", {
+    "title": "Alert example",
+    "content": "alert('Hello World!');"
+  });
+  this.test.comment("And I submit the form");
+  this.click("input[type='submit']");
+});
+
+casper.test.comment("Then I should see a message");
+casper.waitUntilVisible(".message", function() {
+  this.test.assertTextExists("Snippet 'Alert example' saved");
+}, function() {
+  this.debugHTML();
+  this.test.fail("Create snippet message timed out");
+}, 3000);
+
+casper.run(function() {
+  this.test.done();
+});
